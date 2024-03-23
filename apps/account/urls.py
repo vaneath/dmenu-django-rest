e181d97e5a2541fea932d02619dd8600f90e23e4
vaneath <vaneathzz@gmail.com>
2024-03-23 19:18:16 +0700

    close #16 add register view

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from knox import views as knox_views
 
-from .views import LoginView, RegisterUserAPI
+from .views import LoginView, RegisterView
 
 urlpatterns = [
-    path('register/', RegisterUserAPI.as_view()),
+    path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path(r'logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
     path(r'logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,19 +1,27 @@
-from apps.authtoken.serializers import AuthTokenSerializerDecorator
 from django.contrib.auth import login
 from knox.views import LoginView as KnoxLoginView
+from rest_auth.registration.views import RegisterView
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
+
+from apps.authtoken.serializers import AuthTokenSerializerDecorator
 
 from .models import UserDecorator
 from .serializers import (LoginUserSerializer, RegisterUserSerializer,
                           UserSerializer)
 
 
-class RegisterUserAPI(CreateAPIView):
-  query_set = UserDecorator.objects.all()
-  serializer_class = RegisterUserSerializer
-  permission_classes = (AllowAny,)
+class RegisterView(RegisterView):
+
+    def get_response_data(self, user):
+        return AuthTokenSerializerDecorator(data={'user': user, 'token': self.token}).data
+
+    def perform_create(self, serializer):
+        user = serializer.save(self.request)
+        self.token = create_knox_token(None, user, None)
+        complete_signup(self.request._request, user, allauth_settings.EMAIL_VERIFICATION, None)
+        return user
 
 class LoginView(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
